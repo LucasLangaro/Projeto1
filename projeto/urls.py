03b450c87948870f18def6759c58a5ae45cb5eed
Lucas Trindade Langaro <langarolucas@gmail.com>
2022-11-19 19:02:19 -0300

    Criado a pagina 'sobre"

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+#http request
+def my_view(request):
+    return HttpResponse('Uma Linda STRING')
+
+#return http response
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
