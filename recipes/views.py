0ff7251d28425f8f9e54b46499aa6a26b3198e6d
Lucas Trindade Langaro <langarolucas@gmail.com>
2022-11-22 11:06:06 -0300

    criado nova view

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Lucas Trindade Langaro',
     })
+
+def recipe(request,id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Lucas Trindade Langaro',
+    })
